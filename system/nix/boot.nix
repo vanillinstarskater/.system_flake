a6044abcf38262a1e8058e615ccbd8ce8fa584af
Vanillin Starskater <vanillinstarskater@gmail.com>
2025-12-11 22:31:01 -0800

    Switched back to default kernel, because latest is giving me driver issues with nvidia.

--- system/nix/boot.nix
+++ system/nix/boot.nix
@@ -1,9 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
   };
 }
